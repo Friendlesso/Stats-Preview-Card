f866c88a472f1b5cb4ace450b8ad2723e5aec884
Mihailo <mihailoless@gmail.com>
2025-12-20 13:16:45 +0100

    Fix div class for CardImage component

--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -3,10 +3,10 @@ import MobileImg from "../../assets/image-header-mobile.jpg";
 
 export function CardImage() {
   return (
-    <div className="md:w-1/2 sm:w-full lg:w-1/2 overflow-hidden">
+    <div className="md:w-1/2 sm:w-full lg:w-1/2 relative overflow-hidden">
       <img className="hidden sm:block w-full h-full object-cover" src={DesktopImg} alt="Company image" />
       <img className="sm:hidden w-full h-1/2 object-cover" src={MobileImg} alt="Company image" />
-      <div className="absolute w-full h-full bg-(--image-accent-color)"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-(--image-accent-color)"></div>
     </div>
   )
 }
