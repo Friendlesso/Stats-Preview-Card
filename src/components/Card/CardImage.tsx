dcd82daf4b046547409770865f3d5aea42887f71
Mihailo <mihailoless@gmail.com>
2025-12-20 13:18:12 +0100

    Remove duplicate background color div
    Removed duplicate background color div in CardImage component.

--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -4,9 +4,9 @@ import MobileImg from "../../assets/image-header-mobile.jpg";
 export function CardImage() {
   return (
     <div className="md:w-1/2 sm:w-full lg:w-1/2 relative overflow-hidden">
+      <div className="absolute top-0 left-0 w-full h-full bg-(--image-accent-color)"></div>
       <img className="hidden sm:block w-full h-full object-cover" src={DesktopImg} alt="Company image" />
       <img className="sm:hidden w-full h-1/2 object-cover" src={MobileImg} alt="Company image" />
-      <div className="absolute top-0 left-0 w-full h-full bg-(--image-accent-color)"></div>
     </div>
   )
 }
