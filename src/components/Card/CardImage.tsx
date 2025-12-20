4cb2b7ed5d1bd79204c6f58db03b38643772a878
Mihailo <mihailoless@gmail.com>
2025-12-20 13:14:51 +0100

    Add overlay div to CardImage component

--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -6,6 +6,7 @@ export function CardImage() {
     <div className="md:w-1/2 sm:w-full lg:w-1/2 overflow-hidden">
       <img className="hidden sm:block w-full h-full object-cover" src={DesktopImg} alt="Company image" />
       <img className="sm:hidden w-full h-1/2 object-cover" src={MobileImg} alt="Company image" />
+      <div className="absolute w-full h-full bg-(--image-accent-color)"></div>
     </div>
   )
 }
